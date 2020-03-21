6f63c3cc01c3b859b93df1d53aba245b8c580975
Grigorii Horos <horosgrisa@gmail.com>
2020-03-21 13:09:08 +0200

    Optimize plugin

--- pr-zcalc.plugin.zsh
+++ pr-zcalc.plugin.zsh
@@ -1,23 +1,10 @@
 #!/usr/bin/env zsh
-DEPENDENCES_ZSH+=( zpm-zsh/colors )
 
-typeset -g PR_ZCALC_PREFIX
-PR_ZCALC_PREFIX="${PR_ZCALC_PREFIX:-" "}"
-typeset -g PR_ZCALC_SUFFIX
-PR_ZCALC_SUFFIX="${PR_ZCALC_SUFFIX:-""}"
+typeset -g PR_ZCALC_PREFIX="${PR_ZCALC_PREFIX:-" "}"
+typeset -g PR_ZCALC_SUFFIX="${PR_ZCALC_SUFFIX:-""}"
 
+if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+  zpm zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
 
-if (( $+functions[zpm] )); then
-  zpm zpm-zsh/colors,inline
-fi
-
-typeset -g pr_zcalc
-pr_zcalc="${PR_ZCALC_PREFIX}"
-
-if [[ "$CLICOLOR" != "0" ]]; then
-  pr_zcalc+="%{$c[red]%}%1v%{$c_reset%} %{$c[green]%}>%{$c_reset%}"
-else
-  pr_zcalc+="%1v >"
-fi
-
-pr_zcalc+="${PR_ZCALC_SUFFIX}"
+typeset -g pr_zcalc+="${PR_ZCALC_PREFIX}%{$c[red]%}%1v%{$c_reset%} %{$c[green]%}>%{$c_reset%}${PR_ZCALC_SUFFIX}"
