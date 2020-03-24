8bcf4f53c1ae0ddf40f3d01463da6f14509ba035
Grigorii Horos <horosgrisa@gmail.com>
2020-03-24 02:19:50 +0200

    Optimize

--- pr-zcalc.plugin.zsh
+++ pr-zcalc.plugin.zsh
@@ -1,10 +1,6 @@
 #!/usr/bin/env zsh
 
-typeset -g PR_ZCALC_PREFIX="${PR_ZCALC_PREFIX:-" "}"
-typeset -g PR_ZCALC_SUFFIX="${PR_ZCALC_SUFFIX:-""}"
-
-if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-  zpm zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+: ${PR_ZCALC_PREFIX:=" "}
+: ${PR_ZCALC_SUFFIX:=""}
 
 typeset -g pr_zcalc="${PR_ZCALC_PREFIX}%{$c[red]%}%1v%{$c_reset%} %{$c[green]%}>%{$c_reset%}${PR_ZCALC_SUFFIX}"
