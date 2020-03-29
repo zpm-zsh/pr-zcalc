cdae0be414c54f6cdd99e686fd6f3c3b0f307f14
Grigorii Horos <horosgrisa@gmail.com>
2020-03-29 03:20:14 +0300

    Optimize

--- pr-zcalc.plugin.zsh
+++ pr-zcalc.plugin.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-: ${PR_ZCALC_PREFIX:=" "}
-: ${PR_ZCALC_SUFFIX:=""}
+: ${PR_ZCALC_PREFIX:=' '}
+: ${PR_ZCALC_SUFFIX:=''}
 
 typeset -g pr_zcalc="${PR_ZCALC_PREFIX}%{$c[red]%}%1v%{$c_reset%} %{$c[green]%}>%{$c_reset%}${PR_ZCALC_SUFFIX}"
