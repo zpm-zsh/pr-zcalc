15631648fb28b34d0039fd89f6c2bd2bcd0b2a0c
Grigorii Horos <horosgrisa@gmail.com>
2023-08-03 01:04:25 +0300

    Remove prefix/suffix

--- pr-zcalc.plugin.zsh
+++ pr-zcalc.plugin.zsh
@@ -1,6 +1,3 @@
 #!/usr/bin/env zsh
 
-: ${PR_ZCALC_PREFIX:=' '}
-: ${PR_ZCALC_SUFFIX:=''}
-
-typeset -g pr_zcalc="${PR_ZCALC_PREFIX}%{${c[red]}%}%1v%{${c[reset]}%} %{${c[green]}%}>%{${c[reset]}%}${PR_ZCALC_SUFFIX}"
+typeset -g pr_zcalc=" %{${c[red]}%}%1v%{${c[reset]}%} %{${c[green]}%}>%{${c[reset]}%}"
